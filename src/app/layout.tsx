f0df36d42731068555194b8a5ed2533a98a4f33a
and-rs <deserd@protonmail.com>
2025-05-22 19:03:45 -0500

    feat: added favicon setup;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils"
 import type { Metadata } from "next"
 import { ThemeProvider } from "next-themes"
 import { DM_Sans, JetBrains_Mono } from "next/font/google"
+import Head from "next/head"
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -32,6 +33,26 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cn(fontSans.variable, fontMono.variable)}
     >
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body>
         <ThemeProvider
           disableTransitionOnChange
