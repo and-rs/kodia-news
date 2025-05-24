f3fc184b00642592639051f7375501f3719dc795
and-rs <deserd@protonmail.com>
2025-05-23 21:06:36 -0500

    fix: better responsive layout

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -9,7 +9,7 @@ export default async function Header() {
   const tags = await client.fetch<TagsQueryResult>(tagsQuery)
 
   return (
-    <nav className="flex fixed z-50 flex-row gap-4 justify-between items-center p-4 w-full border-b md:justify-between md:p-6 bg-background">
+    <nav className="flex fixed z-50 flex-row flex-wrap gap-4 justify-between items-center p-4 w-full border-b md:justify-between md:p-6 bg-background">
       <div className="flex gap-2 items-center">
         <Link href="/">
           <h2>Kodia News</h2>
@@ -17,7 +17,7 @@ export default async function Header() {
         <ThemeSwitch />
       </div>
       {tags && tags.length > 1 && (
-        <div className="flex gap-2">
+        <div className="flex flex-wrap gap-2">
           {tags.map((tag) => (
             <Link href={`/tags/${tag.slug}`} key={tag._id}>
               <Button variant="link" className="text-md">
